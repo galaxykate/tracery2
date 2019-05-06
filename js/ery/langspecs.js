1d44db6a505fcce0ba989de69423b579fed947f2
Kate <Compton@Main.local>
2019-05-06 16:09:06 -0700

    create recursive object structure

--- js/ery/langspecs.js
+++ js/ery/langspecs.js
@@ -2,20 +2,27 @@
 // celery, cemetery, chancery, cheery, chicanery, confectionery, creamery, crockery, cutlery, delivery, drapery, embroidery, emery, every, fiery,finery, flattery, flowery, frippery, gallery, greenery, imagery, livery, lottery, mastery, millinery, misery, monastery, mystery, nursery, ornery, peppery, periphery, pottery, refinery, robbery, scenery, sorcery, stationery, tannery, tapestry, witchery, aery, archery, artery, bakery, bravery
 let languageSpecs = {
 	traceryOG: {
+		id: "traceryOG",
+		types: {
+			RuleSet: ["string(tracery)", "array of string(tracery)"],
+			Grammar: "dictionary of RuleSet"
+		},
+		origin: "Grammar"
 
 	},
 	tracery2: {
-
+		id: "tracery2"
 	},
 	chancery: {
-
+		id: "chancery"
 	},
 	// A language for idle games
 	filigree: {
-
+		id: "filigree"
 	},
 
-	filigree: {
+	stationery: {
+		id: "stationery"
 
 	}
 }
--- js/ui-lang.js
+++ js/ui-lang.js
@@ -1,18 +1,91 @@
-var langViewer = new Vue({
+// A view of an object
+
+Vue.component('obj-view', {
+	template: `
+	<div v-bind:class="'objview objview-' + getType(obj)">
+		
+		<div v-if="getType(obj) === 'object'"
+			class="objview-row" v-for="(value, name) in obj">
+			<div class="objview-row-label" >{{name}}</div>:
+			<div class="objview-row-content">
+			
+				<obj-view :obj="value">
+				</obj-view>
+			</div>
+		</div>
+
+		<div v-if="getType(obj) ==='array'">
+			ARRAY
+		</div>
+
+		<div v-if="getType(obj) ==='string'">
+			STRING:{{obj}}
+		</div>
+	</div>
+	`,
+	props: ["obj"],
+	methods: {
+		getType: function() {
+			if (typeof this.obj === "object") {
+				if (Array.isArray(this.obj))
+					return "array";
+				return "object"
+			}
+			return typeof this.obj
+		}
+	}
+})
+
+Vue.component('array-view', {
 	template: `
 	<div>
-	<select v-model="selected">
-	${Object.keys(languageSpecs).map(key => "<option>" + key + "</option>").join("")}
-	</select>
-	{{selected}}
+		
+		<div class="arrayview-item" v-for="item in arr">
+			{{item}}
+		</div>
 	</div>
-
 	`,
+	props: ["arr"]
+})
+
+var langViewer = new Vue({
+	template: `
+	<div class="panel language-view">
+		<div class="header">
+			<div class="header-main">
+				{{selected}}
+			</div>
+			<div class="header-controls">
+				<select v-model="selected" @change="onChange">
+					<option v-for="(lang, key) in languageSpecs"
+						v-bind:value="lang"
+					>
+					{{key}}
+					</option>
+				</select>
+				
+			</div>
+		</div>
+		<div class="content">
+			<obj-view :obj="selected"></obj-view>
+		</div>
+	</div>
+	`,
+
+	methods: {
+		onChange: function() {
+			console.log(this.selected)
+		}
+	},
 
 
 	el: "#spec",
-	data: {
-		selected: ''
-	}
+	data: function() {
+		return {
 
+			languageSpecs: languageSpecs,
+			selected: languageSpecs.traceryOG
+		}
+	}
 })
+

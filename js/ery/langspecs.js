6286645ba7a06c91ae00fd4b56915d16e9a69ea7
Kate <Compton@Main.local>
2019-05-07 17:34:28 -0700

    Split all the ery object types into separate obj-views, for arrays, dictionaries, strings, etc

--- js/ery/langspecs.js
+++ js/ery/langspecs.js
@@ -1,18 +1,253 @@
 // Language specifications for tracery, chancery...and more!
 // celery, cemetery, chancery, cheery, chicanery, confectionery, creamery, crockery, cutlery, delivery, drapery, embroidery, emery, every, fiery,finery, flattery, flowery, frippery, gallery, greenery, imagery, livery, lottery, mastery, millinery, misery, monastery, mystery, nursery, ornery, peppery, periphery, pottery, refinery, robbery, scenery, sorcery, stationery, tannery, tapestry, witchery, aery, archery, artery, bakery, bravery
-let languageSpecs = {
+let objCount = 0
+
+function expandObject(raw) {
+	// Expand this raw object into an editable blackboard-style boject,
+	// Where all elements have object wrappers to track state/type (and allow vue editing)
+
+	let obj = {
+		id: objCount++,
+	}
+	// All objects are dictionaries, because we don't have an object property list
+	switch (typeof raw) {
+		case "object":
+			if (Array.isArray(raw)) {
+				obj.type = "array"
+				obj.items = raw.map(expandObject)
+			} else {
+				obj.type = "dictionary"
+				obj.props = mapObject(raw, expandObject)
+			}
+			break;
+		case "number":
+		case "boolean":
+		case "string":
+			obj.type = typeof raw
+			obj.value = raw
+			break;
+
+
+	}
+	return obj
+}
+
+
+let languageSpecsRaw = {
 	traceryOG: {
+		editable: true,
+		version: 1.0,
 		id: "traceryOG",
 		types: {
 			RuleSet: ["string(tracery)", "array of string(tracery)"],
 			Grammar: "dictionary of RuleSet"
 		},
 		animals: {
-			mammals: ["okapi", "corgi","rhinoceros", "porpoise"],
+			mammals: ["okapi", "corgi", "rhinoceros", "porpoise"],
 			birds: ["emu", "eagle", "flamingo"],
 			fish: ["catfish", "pufferfish"],
 		},
-		origin: "Grammar"
+		// origin: "Grammar",
+		// herbs: {
+		// 	"description": "A list of herbs and spices, and mixtures of the two.",
+		// 	"herbs": [
+		// 		"Angelica",
+		// 		"Basil",
+		// 		"Holy Basil",
+		// 		"Thai Basil",
+		// 		"Bay leaf",
+		// 		"Indian Bay leaf",
+		// 		"Boldo",
+		// 		"Borage",
+		// 		"Chervil",
+		// 		"Chives",
+		// 		"Garlic Chives",
+		// 		"Cicely",
+		// 		"Coriander leaf",
+		// 		"Cilantro",
+		// 		"Bolivian Coriander",
+		// 		"Vietnamese Coriander",
+		// 		"Culantro",
+		// 		"Cress",
+		// 		"Curry leaf",
+		// 		"Dill",
+		// 		"Epazote",
+		// 		"Hemp",
+		// 		"Hoja santa",
+		// 		"Houttuynia cordata",
+		// 		"Hyssop",
+		// 		"Jimbu",
+		// 		"Kinh gioi",
+		// 		"Lavender",
+		// 		"Lemon balm",
+		// 		"Lemon grass",
+		// 		"Lemon myrtle",
+		// 		"Lemon verbena",
+		// 		"Limnophila aromatica",
+		// 		"Lovage",
+		// 		"Marjoram",
+		// 		"Mint",
+		// 		"Mugwort",
+		// 		"Mitsuba",
+		// 		"Oregano",
+		// 		"Parsley",
+		// 		"Perilla",
+		// 		"Rosemary",
+		// 		"Rue",
+		// 		"Sage",
+		// 		"Savory",
+		// 		"Sansho",
+		// 		"Shiso",
+		// 		"Sorrel",
+		// 		"Tarragon",
+		// 		"Thyme",
+		// 		"Woodruff"
+		// 	],
+		// 	"spices": [
+		// 		"Aonori",
+		// 		"Ajwain",
+		// 		"Allspice",
+		// 		"Amchoor",
+		// 		"Anise",
+		// 		"Star Anise",
+		// 		"Asafoetida",
+		// 		"Camphor",
+		// 		"Caraway",
+		// 		"Cardamom",
+		// 		"Black Cardamom",
+		// 		"Cassia",
+		// 		"Celery powder",
+		// 		"Celery seed",
+		// 		"Charoli",
+		// 		"Chenpi",
+		// 		"Cinnamon",
+		// 		"Clove",
+		// 		"Coriander seed",
+		// 		"Cubeb",
+		// 		"Cumin",
+		// 		"Nigella sativa",
+		// 		"Bunium persicum",
+		// 		"Dill",
+		// 		"Dill seed",
+		// 		"Fennel",
+		// 		"Fenugreek",
+		// 		"Fingerroot",
+		// 		"Greater Galangal",
+		// 		"Lesser Galangal",
+		// 		"Garlic",
+		// 		"Ginger",
+		// 		"Aromatic Ginger",
+		// 		"Golpar",
+		// 		"Grains of Paradise",
+		// 		"Grains of Selim",
+		// 		"Horseradish",
+		// 		"Juniper berry",
+		// 		"Kokum",
+		// 		"Korarima",
+		// 		"Dried Lime",
+		// 		"Liquorice",
+		// 		"Litsea cubeba",
+		// 		"Mace",
+		// 		"Mango-ginger",
+		// 		"Mastic",
+		// 		"Mahlab",
+		// 		"Black Mustard",
+		// 		"Brown Mustard",
+		// 		"White Mustard",
+		// 		"Nigella",
+		// 		"Njangsa",
+		// 		"Nutmeg",
+		// 		"Alligator Pepper",
+		// 		"Brazilian Pepper",
+		// 		"Chili Pepper",
+		// 		"Cayenne pepper",
+		// 		"Paprika",
+		// 		"Long Pepper",
+		// 		"Peruvian Pepper",
+		// 		"East Asian Pepper",
+		// 		"Sichuan Pepper",
+		// 		"Sansho",
+		// 		"Tasmanian Pepper",
+		// 		"Black Peppercorn",
+		// 		"Green Peppercorn",
+		// 		"White Peppercorn",
+		// 		"Pomegranate seed",
+		// 		"Poppy seed",
+		// 		"Radhuni",
+		// 		"Rose",
+		// 		"Saffron",
+		// 		"Salt",
+		// 		"Sarsaparilla",
+		// 		"Sassafras",
+		// 		"Sesame",
+		// 		"Shiso",
+		// 		"Sumac",
+		// 		"Tamarind",
+		// 		"Tonka bean",
+		// 		"Turmeric",
+		// 		"Uzazi",
+		// 		"Vanilla",
+		// 		"Voatsiperifery",
+		// 		"Wasabi",
+		// 		"Yuzu",
+		// 		"Zedoary",
+		// 		"Zereshk",
+		// 		"Zest"
+		// 	],
+		// 	"mixtures": [
+		// 		"Adjika",
+		// 		"Advieh",
+		// 		"Baharat",
+		// 		"Beau Monde seasoning",
+		// 		"Berbere",
+		// 		"Bouquet garni",
+		// 		"Buknu",
+		// 		"Chaat masala",
+		// 		"Chaunk",
+		// 		"Chili powder",
+		// 		"Crab boil",
+		// 		"Curry powder",
+		// 		"Doubanjiang",
+		// 		"Douchi",
+		// 		"Duqqa",
+		// 		"Fines herbes",
+		// 		"Five-spice powder",
+		// 		"Garam masala",
+		// 		"Garlic powder",
+		// 		"Garlic salt",
+		// 		"Gochujang",
+		// 		"Harissa",
+		// 		"Hawaij",
+		// 		"Herbes de Provence",
+		// 		"Idli podi",
+		// 		"Jamaican jerk spice",
+		// 		"Khmeli suneli",
+		// 		"Lemon pepper",
+		// 		"Mitmita",
+		// 		"Mixed spice",
+		// 		"Montreal steak seasoning",
+		// 		"Mulling spices",
+		// 		"Old Bay Seasoning",
+		// 		"Onion powder",
+		// 		"Panch phoron",
+		// 		"Persillade",
+		// 		"Powder-douce",
+		// 		"Pumpkin pie spice",
+		// 		"Qâlat daqqa",
+		// 		"Quatre épices",
+		// 		"Ras el hanout",
+		// 		"Recado rojo",
+		// 		"Sharena sol",
+		// 		"Shichimi",
+		// 		"Tabil",
+		// 		"Tandoori masala",
+		// 		"Vadouvan",
+		// 		"Yuzukosho",
+		// 		"Za'atar"
+		// 	]
+		// }
+
+
 
 	},
 	tracery2: {
@@ -31,3 +266,7 @@ let languageSpecs = {
 
 	}
 }
+
+let languageSpecs = mapObject(languageSpecsRaw, expandObject)
+
+console.log(languageSpecs)
--- js/ui-lang.js
+++ js/ui-lang.js
@@ -1,63 +1,105 @@
 // A view of an object
 
+
 Vue.component('obj-view', {
 	template: `
-	<div v-bind:class="'objview objview-' + getType(obj)">
+	<div v-bind:class="'objview objview-' + obj.type + '-holder'">
+	
+		<obj-dictionary
+			:obj="obj" :rootObj="rootObj" 
+			v-if="obj.type === 'dictionary'">
+		</obj-dictionary>
 		
-		<div class="objview-row" 
-			v-if="getType(obj) === 'object'"
-			v-for="(value, name) in obj"
-		>
-			<div class="objview-row-label" >{{name}}</div>:
-			<div class="objview-row-content">
-			
-				<obj-view :obj="value">
-				</obj-view>
-			</div>
-		</div>
 
-		<div class="objview-item"
-			v-if="getType(obj) ==='array'"
-			v-for="item in obj"
-		>
-			<obj-view :obj="item">
-			</obj-view>
-		</div>
+		<obj-array
+			:obj="obj" :rootObj="rootObj" 
+			v-else-if="obj.type === 'array'">
+		</obj-array>
+		
 
-		<pre v-if="getType(obj) ==='string'" class="objview-string">{{obj}}</pre>
-	</div>
+		<obj-string
+			:obj="obj" :rootObj="rootObj" 
+			v-else-if="obj.type === 'string'">
+		</obj-string>
+		
+
+		<div 
+			:obj="obj" :rootObj="rootObj" 
+			v-else>
+			UNKNOWN TYPE: {{obj.type}}
+		</div>
+		
+
+	</div>`,
+	
+	props: ["obj", "rootObj"],
+})
+
+
+
+Vue.component('obj-dictionary', {
+	props: ["obj", "rootObj"],
+	template: `<div 
+				class="objview-dictionary"
+				>
+					<div v-for="(val,key) in obj.props" 
+						v-bind:key="key"
+						class="objview-row"
+					>
+						<div class="objview-row-label">{{key}}</div>:
+							<obj-view :obj="val" :objRoot="rootObj"></obj-view>
+					</div>
+				</div>
 	`,
-	props: ["obj"],
+})
+
+Vue.component('obj-array', {
+	props: ["obj", "rootObj"],
+	template: `<div 
+				class="objview-array"
+				>
+					<obj-view 
+						v-for="(item, index) in obj.items"
+						v-bind:key="index"
+						:obj="item" :objRoot="rootObj">
+					</obj-view>
+				</div>
+	`,
+})
+
+
+Vue.component('obj-string', {
+	props: ["obj", "rootObj"],
+	
+	template: `<pre 
+				@input="onInput"
+				@paste.stop.prevent="onPaste"
+				class="objview-string"
+				contenteditable="true"
+				>{{obj.value}}</pre>
+	`,
+
 	methods: {
-		getType: function() {
-			if (typeof this.obj === "object") {
-				if (Array.isArray(this.obj))
-					return "array";
-				return "object"
-			}
-			return typeof this.obj
-		}
-	}
+		onInput: function(e) {
+			 console.log("edit: "+ e.target.innerText);
+			
+		},
+		onPaste: function(e) {
+			console.log("paste: "+ e.target.innerText);
+		
+	        var plaintext = e.clipboardData.getData('text/plain');
+	         document.execCommand('inserttext', false, plaintext);
+		},
+	},
 })
 
-Vue.component('array-view', {
-	template: `
-	<div>
-		
-		<div class="arrayview-item" v-for="item in arr">
-			{{item}}
-		</div>
-	</div>
-	`,
-	props: ["arr"]
-})
 
 var langViewer = new Vue({
 	template: `
 	<div class="panel language-view">
 		<div class="header">
 			<div class="header-main">
-				{{selected}}
+				{{selected.id}}
 			</div>
 			<div class="header-controls">
 				<select v-model="selected" @change="onChange">
@@ -92,4 +134,3 @@ var langViewer = new Vue({
 		}
 	}
 })
-
